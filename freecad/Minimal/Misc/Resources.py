ccee45a017eb5cbd5b453001f53448f49c26b991
ᴩʜᴏɴᴇᴅʀᴏɪᴅ <73050054+PhoneDroid@users.noreply.github.com>
2026-01-17 06:03:43 -0500

    Add Resources helper

---
+++ freecad/Minimal/Misc/Resources.py
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# SPDX-FileNotice: Part of the StandardBeams addon.
+
+import freecad.Minimal as module
+from importlib import resources
+
+
+icons = resources.files(module) / 'Resources/Icons'
+
+
+def asIcon ( name : str ):
+
+    file = name + '.svg'
+
+    icon = icons / file
+
+    with resources.as_file(icon) as path:
+        return str( path )
