7a9dcefa70a0ba59ebf17f035d7bd90c1680c94a
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-03-27 12:16:43 -0400

    Replace QT wrapper

--- freecad/Minimal/Qt/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: LGPL-3.0-or-later
-# SPDX-FileNotice: Part of the Minimal addon.
-
-from . import Widgets as QtWidgets
-from . import Core as QtCore
-from . import Gui as QtGui
--- freecad/Minimal/init_gui.py
+++ freecad/Minimal/init_gui.py
@@ -2,4 +2,11 @@
 # SPDX-FileNotice: Part of the Minimal addon.
 
 
+from PySide6 import QtWidgets , QtCore , QtGui
+from FreeCAD import Console , Gui , Qt
+
+
+translate = Qt.translate
+
+
 print('freecad/Minimal/init_gui.py')
